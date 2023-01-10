67d834705fc0637ae30dc2af781fb977f1f51368
Ryan Canton <ryancanton999@gmail.com>
2023-01-09 17:19:47 -0700

    Feat: Add functionality to chef show page

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,14 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+
+  def create
+    Dish.create(dish_params)
+    redirect_to chef_path(params[:chef_id])
+  end
+
+private
+  def dish_params
+    params.permit(:chef_id, :name, :description)
+  end
 end
