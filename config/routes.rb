e5545baec0fb5e38de0664b671411aea1573b568
Ryan Canton <ryancanton999@gmail.com>
2023-01-09 18:11:14 -0700

    Feat: Finished user story 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :dishes, only: :show
   patch '/dishes', to: 'dishes#update'
 
-  get "/chefs/:id/ingredients", to: 'chefs_ingredients#index', as: 'chef_ingredients'
+  get "/chefs/:id/ingredients", to: 'chef_ingredients#index', as: 'chef_ingredients'
   resources :chefs, only: :show
 end
